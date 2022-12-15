0ab963233787234dfe6d8d21b42a6f2960e324e8
Henrique França <henriqsf@gmail.com>
2022-12-15 08:16:28 -0300

    feat: Configuração Inicial

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack')
+
+module.exports = {
+    //onde a aplicção sera inicializada (ponto de entrada)
+    entry: './ex/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public' 
+    }
+}
+
+
